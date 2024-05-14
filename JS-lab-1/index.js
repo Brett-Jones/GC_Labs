44eef42575a48cf06853477d764bdc40293a8526
Kolstrike <kolstrike1@gmail.com>
2024-05-13 21:39:57 -0400

    JS lab1, complete

--- JS-lab-1/index.js
+++ JS-lab-1/index.js
@@ -1,21 +1,22 @@
+let firstName ="Brett"
+let lastName = "Jones"
+let fullName = firstName + lastName
+let population = 620376
+let javaScriptIsCool = true
+let colors = "Red, Orange, Yellow, Green, Blue, Indigo, Purple"
 
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+    
+}
 
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+    
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log (fullName)
+console.log (population * 3)
 
 
 
