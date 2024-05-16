58f2fd8db2346f95de8e4ef7f36a9b8fa0458efe
Kolstrike <kolstrike1@gmail.com>
2024-05-16 16:34:19 -0400

    cleaing up code

--- JS-lab-3/index.js
+++ JS-lab-3/index.js
@@ -18,40 +18,6 @@ if (name1.length > name2.length && name1.length > name3.length) {
     console.log(name2 + " and " + name3 + " tie for the longest name.");
 } 
 
-// if (name1.length > name2.length && name1.length === name3.length) {
-//     console.log(name1) // dwight
-
-// } else if (name2.length < name1.length && name2.length < name3.length) {
-//     console.log(name2) // jim
-
-// } else if(name3.length === name1.length && name3.length > name2.length) {
-//     console.log(name3) // michel
-
-// } else if(name1.length === name3.length && name2.length < name1.length) {
-//     console.log(name1 + " " + name3) // dwight + michel
-
-// } else if(name3.length > name2.length && name1.length > name2.length) {
-//     console.log(name1 + " " + name2 + " " + name3) // dwight, jim
-
-// } else if(name2.length < name3.length && name3.length === name1.length) {
-//     console.log(name2 + " " + name3) //jim, michel
-
-// } else if(name3.length > name2.length && name2.length < name3.length) {
-//     console.log(name1 + " " + name2 + " " + name3) // dwight, jim, michel
-
-// } else {
-//     console.log("Everyone is here")
-// }
-
-
-
-
-// dwight, jim, michel = dwight
-// dwight, michel, jim = michel
-// jim, michel, dwight = jim
-// jim, dwight, michel = dwight, michel
-// michael, dwight, jim = jim, michel
-// michael, jim dwight = dwight, jim
 
 
 
