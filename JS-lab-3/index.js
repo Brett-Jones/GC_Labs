811d8ac508c853399400b1b49b0e125fd442f0e3
Brett.Jones <kolstrike1@gmail.com>
2024-05-16 12:24:42 -0400

    updated conditions

--- JS-lab-3/index.js
+++ JS-lab-3/index.js
@@ -1,31 +1,47 @@
 let name1 = "Dwight";
 let name2 = "Jim";
-let name3 = "Michel";
+let name3 = "Michael";
 
-if (name1.length > name2.length && name1.length === name3.length) {
-    console.log(name1) // dwight
+if (name1.length > name2.length && name1.length > name3.length) {
+    console.log(name1 + " has the longest name.");
+} else if (name2.length > name1.length && name2.length > name3.length) {
+    console.log(name2 + " has the longest name.");
+} else if (name3.length > name1.length && name3.length > name2.length) {
+    console.log(name3 + " has the longest name.");
+} else if(name1.length === name2.length && name1.length === name3.length && name2.length === name3.length) {
+    console.log("All three names, " + name1 + ", " + name2 + ", " + name3 + " are the same length.");
+} else if (name1.length === name2.length && name1.length !== name3.length && name2.length !== name3.length) {
+    console.log(name1 + " and " + name2 + " tie for the longest name.");
+} else if (name1.length === name3.length && name1.length !== name2.length && name2.length !== name3.length) {
+    console.log(name1 + " and " + name3 + " tie for the longest name.");
+} else if (name2.length === name3.length && name1.length !== name2.length && name1.length !== name3.length) {
+    console.log(name2 + " and " + name3 + " tie for the longest name.");
+} 
 
-} else if (name2.length < name1.length && name2.length < name3.length) {
-    console.log(name2) // jim
+// if (name1.length > name2.length && name1.length === name3.length) {
+//     console.log(name1) // dwight
 
-} else if(name3.length === name1.length && name3.length > name2.length) {
-    console.log(name3) // michel
+// } else if (name2.length < name1.length && name2.length < name3.length) {
+//     console.log(name2) // jim
 
-} else if(name1.length === name3.length && name2.length < name1.length) {
-    console.log(name1 + " " + name3) // dwight + michel
+// } else if(name3.length === name1.length && name3.length > name2.length) {
+//     console.log(name3) // michel
 
-} else if(name3.length > name2.length && name1.length > name2.length) {
-    console.log(name1 + " " + name2 + " " + name3) // dwight, jim
+// } else if(name1.length === name3.length && name2.length < name1.length) {
+//     console.log(name1 + " " + name3) // dwight + michel
 
-} else if(name2.length < name3.length && name3.length === name1.length) {
-    console.log(name2 + " " + name3) //jim, michel
+// } else if(name3.length > name2.length && name1.length > name2.length) {
+//     console.log(name1 + " " + name2 + " " + name3) // dwight, jim
 
-} else if(name3.length > name2.length && name2.length < name3.length) {
-    console.log(name1 + " " + name2 + " " + name3) // dwight, jim, michel
+// } else if(name2.length < name3.length && name3.length === name1.length) {
+//     console.log(name2 + " " + name3) //jim, michel
 
-} else {
-    console.log("Everyone is here")
-}
+// } else if(name3.length > name2.length && name2.length < name3.length) {
+//     console.log(name1 + " " + name2 + " " + name3) // dwight, jim, michel
+
+// } else {
+//     console.log("Everyone is here")
+// }
 
 
 
