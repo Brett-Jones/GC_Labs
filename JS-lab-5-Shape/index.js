1876674f945d002eeb8d9e5241001de1bdfa764f
Kolstrike <kolstrike1@gmail.com>
2024-05-21 18:40:32 -0400

    Lab 5 shapes fully done

---
+++ JS-lab-5-Shape/index.js
@@ -0,0 +1,36 @@
+
+function getAreaOfCircle(radius) {
+	const area = Math.PI * radius^2;
+    return area;
+}
+
+console.log(getAreaOfCircle(13));
+
+function getCircumferenceOfCircle(radius) {
+	const around = 2 * Math.PI * radius;
+    return around;
+}
+ console.log(getCircumferenceOfCircle(9));
+
+function getAreaOfSquare(side) {
+	const area = side * side;
+    return area;
+}
+
+console.log(getAreaOfSquare(22));
+
+function getAreaOfTriangle(base, height) {
+	const area = height * base / 2;
+    return area;
+}
+
+console.log(getAreaOfTriangle(7, 11));
+
+
+
+
+
+
+
+
+
