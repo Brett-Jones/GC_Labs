a0a2c8e7c5389ebf1777cbaa78693cec444bf6b1
Kolstrike <kolstrike1@gmail.com>
2024-05-28 22:29:20 -0400

    finished JS lab 6

--- JS-lab-6-StuSub/index.js
+++ JS-lab-6-StuSub/index.js
@@ -1,3 +1,4 @@
+/** @format */
 
 const submissions = [
 	{
@@ -22,64 +23,63 @@ const submissions = [
 	},
 ];
 
-const addSubmission = 
-    (submissions, 
-    newName,
-    newScore
-    ) => {submissions.push({
-            name: "Jake",
-            score: 69,
-            passed: true
-        })
-    }
+const addSubmission = (array, newName, newScore) => {
+	array.push({
+		name: newName,
+		score: newScore,
+		passed: newScore >= 60,
+	});
+};
 
-const deleteSubmissionByIndex = (submissions, []) => {submissions.score.slice[1]};
+const deleteSubmissionByIndex = (array, index) => {
+	array.splice(index, 1);
+};
 
-const deleteSubmissionByName = (submissions, []) => {submissions.name.slice(2)};
+const deleteSubmissionByName = (array, name) => {
+	array.splice(
+		array.findIndex(record => record.name === name),
+		1
+	);
+};
 
-const editSubmission = (submissions, [], [score]) => {}
+const editSubmission = (array, index, score) => {
+	const updateRecord = {
+		...array[index],
+		score: score,
+		passed: score >= 60,
+	};
+	array[index] = updateRecord;
+};
 
+const findSubmissionByName = (array, name) => {
+	return array.find(record => record.name === name);
+};
 
+const findLowestScore = array => {
+	let result = array[0];
+	let smallest = array[0].score;
+	for (let i = 1; i < array.length; i++) {
+		if (array[i].score < smallest) {
+			smallest = array[i].score;
+			result = array[i];
+		}
+	}
+	return result;
+};
 
+const findAverageScore = array => {
+	let total = 0;
+	for (let i = 0; i < array.length; i++) {
+		total += array[i].score;
+	}
+	return total / array.length;
+};
 
+const filterPassing = array => {
+	return array.filter(record => record.passed === true);
+};
 
+const filter90AndAbove = array => {
+	return array.filter(record => record.score >= 90);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ref only lol
-// console.log(submissions[0]);
-// submissions.[](0)
-// submissions.squarebrackets(0)
-
-
-// const newStudent = {
-// 	name: "Jill",
-// 	score: 88,
-// 	passed: true,
-// 	classes: [
-// 		{
-// 			firstPeriod: {
-// 				subject: "Math",
-// 				teacher: "AJ Tran",
-// 			},
-// 		},
-// 	],
-// };
